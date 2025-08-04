2c5603412082dcfb7dc496eaabc6508409393292
Jagoda Pogroszewsla <pogroszewskadesign@gmail.com>
2025-08-04 11:35:28 +0200

    footer and contact

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,10 +2,8 @@ import styles from './Footer.module.scss';
 
 const Footer = () => {
     return (
-        <footer className={styles.footer}>
-   
+        <footer className={styles.fullWidthBackground}>
         <p className={styles.text} >Copyright @ 2025 Jagoda Pogroszewska - All rights reserved.</p>
-    
        </footer>
     );
 };
